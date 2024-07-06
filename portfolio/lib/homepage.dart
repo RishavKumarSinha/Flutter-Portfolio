d4fa4e6cd01aaa6ee2e93eead64e5d617b4840be
Rishav Kumar Sinha <sinharishavkumar@gmail.com>
2024-07-06 20:21:25 +0530

    Nav Bar Design Completed

--- portfolio/lib/homepage.dart
+++ portfolio/lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,153 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Add your main content here
+          Positioned(
+            top: 20,
+            left: 15,
+            right: 15,
+            child: Center(
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 600),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFE9C8),
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  child: GNav(
+                    tabBackgroundColor: const Color(0xFFFFCC80),
+                    tabActiveBorder: Border.all(color: Colors.black, width: 1),
+                    gap: 8,
+                    activeColor: Colors.black,
+                    tabs: [
+                      GButton(
+                        icon: Icons.person,
+                        text: 'About',
+                        textStyle: GoogleFonts.lilitaOne(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 18,
+                          shadows: [
+                            const Shadow( // bottomLeft
+                              offset: Offset(-1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // bottomRight
+                              offset: Offset(1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topRight
+                              offset: Offset(1.5, 1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topLeft
+                              offset: Offset(-1.5, 1.5),
+                              color: Colors.black
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      ),
+                      GButton(
+                        icon: Icons.folder,
+                        text: 'Projects',
+                        textStyle: GoogleFonts.lilitaOne(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            const Shadow( // bottomLeft
+                              offset: Offset(-1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // bottomRight
+                              offset: Offset(1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topRight
+                              offset: Offset(1.5, 1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topLeft
+                              offset: Offset(-1.5, 1.5),
+                              color: Colors.black
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      ),
+                      GButton(
+                        icon: Icons.emoji_events,
+                        text: 'Certifications',
+                        textStyle: GoogleFonts.lilitaOne(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            const Shadow( // bottomLeft
+                              offset: Offset(-1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // bottomRight
+                              offset: Offset(1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topRight
+                              offset: Offset(1.5, 1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topLeft
+                              offset: Offset(-1.5, 1.5),
+                              color: Colors.black
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      ),
+                      GButton(
+                        icon: Icons.contact_page,
+                        text: 'Contact',
+                        textStyle: GoogleFonts.lilitaOne(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            const Shadow( // bottomLeft
+                              offset: Offset(-1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // bottomRight
+                              offset: Offset(1.5, -1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topRight
+                              offset: Offset(1.5, 1.5),
+                              color: Colors.black
+                            ),
+                            const Shadow( // topLeft
+                              offset: Offset(-1.5, 1.5),
+                              color: Colors.black
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
