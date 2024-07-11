fa78a6c23eda783efad44b917e293ffb5414b396
Rishav Kumar Sinha <sinharishavkumar@gmail.com>
2024-07-11 11:29:34 +0530

    System Orientation Locked

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:portfolio/homepage.dart';
 
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
